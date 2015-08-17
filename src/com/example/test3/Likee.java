cc66dc0b20b5c51b90738ce40500653fa1e41606
gautamb0 <the_mongoose_50@hotmail.com>
2015-08-17 12:18:17 -0700

    Added Likes

---
+++ src/com/example/test3/Likee.java
@@ -0,0 +1,37 @@
+package com.example.test3;
+
+import java.io.IOException;
+import java.util.Date;
+import java.util.List;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.googlecode.objectify.Key;
+import com.googlecode.objectify.ObjectifyService;
+
+public class Likee extends HttpServlet {
+	  
+	  public void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+		  ImageRecord imageRecord; 	
+
+		  String ancestor = "default";
+		  
+		  String sid = req.getParameter("sid");
+		  String user_nickname = req.getParameter("user");
+
+		  
+		  System.out.println("sid: "+ sid);
+		  System.out.println("user_nickname: "+ user_nickname);
+		  Long id = Long.parseLong(sid);
+		  Key<Gallery> theBook = Key.create(Gallery.class, ancestor);
+		  imageRecord = ObjectifyService.ofy().load().type(ImageRecord.class).parent(theBook).id(id).now();
+
+		  imageRecord.likes++;
+		  imageRecord.likedUsers.add(user_nickname);
+		  System.out.print(imageRecord.likedUsers);
+		  ObjectifyService.ofy().save().entity(imageRecord).now();		  
+		  
+	  }
+}
