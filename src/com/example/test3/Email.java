e8bd02fc949f20868829bccba325bc87c7589503
gautamb0 <the_mongoose_50@hotmail.com>
2016-01-25 13:20:55 -0800

    Added email functionality

---
+++ src/com/example/test3/Email.java
@@ -0,0 +1,44 @@
+package com.example.test3;
+
+import java.io.IOException;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import java.util.Properties;
+
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.AddressException;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+
+public class Email extends HttpServlet {
+	
+	public void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+		Properties props = new Properties();
+        Session session = Session.getDefaultInstance(props, null);
+
+        String msgBody = "...";
+
+        try {
+            Message msg = new MimeMessage(session);
+            msg.setFrom(new InternetAddress("spherify@cobalt-list-99804.appspotmail.com", "Spherify"));
+            msg.addRecipient(Message.RecipientType.TO,
+                             new InternetAddress("000gautam000@gmail.com", "Mr. User"));
+            msg.setSubject("Your Example.com account has been activated");
+            msg.setText(msgBody);
+            Transport.send(msg);
+
+        } catch (AddressException e) {
+            // ...
+        } catch (MessagingException e) {
+            // ...
+        }
+        
+	}
+	
+}
